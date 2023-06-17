b8ea3de85c8ac6df12d56f97f57ae8f615d5efc0
hantae <85547680+hantae96@users.noreply.github.com>
2023-06-17 14:51:02 +0900

    Initial commit

---
+++ src/main/java/com/toy/itx_subway/ItxSubwayApplication.java
@@ -0,0 +1,13 @@
+package com.toy.itx_subway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItxSubwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItxSubwayApplication.class, args);
+    }
+
+}
